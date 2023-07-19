4c2cf38db90e110e603631fa8e8aff734f2a665b
Violet Mehrzad <viomehr@gmail.com>
2023-07-19 15:01:20 +0330

    Initial Commit; implemented first JS excersize (js-01)

---
+++ js-01/index.js
@@ -0,0 +1,13 @@
+let num1 = +prompt("Please Enter the First Number!")
+let num2 = +prompt("Please Enter the Second Number!")
+let sum = num1 + num2
+
+console.log(num1, num2)
+console.log(typeof num1, typeof num2)
+console.log(num1, "+", num2, "=", sum)
+console.log(typeof sum)
+
+document.write("No.01: ", num1)
+document.write(" , No.02: ", num2)
+document.write(" , Sum: ",num1, " + ", num2, " = ", sum)
+
