aabb53787c2e95afed46b31e13db1fb3f10bc801
Violet Mehrzad <viomehr@gmail.com>
2023-07-29 16:55:52 +0330

    edit/js-exercize-03/edit-tasks-02-and-03

--- js-03/task_03-02.js
+++ js-03/task_03-02.js
@@ -1,6 +1,9 @@
 const userText = prompt('Please Enter Text In Lowercase Letters!')
 
 function textUpperCase(x) {
+    if (!x) {
+        throw Error("Text Is Required!")
+    }
     return x.toUpperCase()
 }
 
--- js-03/task_03-03.js
+++ js-03/task_03-03.js
@@ -5,18 +5,18 @@ const userScore_4 = +prompt("Enter Scores!")
 const userScore_5 = +prompt("Enter Scores!")
 
 function average(a, b, c, d, e) {
-    if (!isNaN(a + b + c + d + e)) {
-        return (a + b + c + d + e) / 5
+    const sum = a + b + c + d + e
+    const avg = sum / 5
+    if (!isNaN(sum)) {
+        switch (avg) {
+            case avg < 12:
+                return avg + ' - RESULT: FAILED';
+            default:
+                return avg + ' - RESULT: PASSED';
+        }
     }
-    return 'ERR! INVALID NUMBERS!'
+    return 'ERR! INVALID NUMBERS!' 
 }
 
 const userAverage = average(userScore_1, userScore_2, userScore_3, userScore_4, userScore_5)
 console.log(userAverage)
-
-
-if (userAverage >= 12) {
-    console.log('Passed!')
-} else {
-    console.log('Failed!')
-}
